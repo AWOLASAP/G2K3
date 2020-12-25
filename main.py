da82e46815170afdd5ae9ff17d08222fc9717412
Griffin Walraven <griffin.walraven@gmail.com>
2020-12-24 22:18:08 -0800

    done more

--- main.py
+++ main.py
@@ -5,12 +5,21 @@
 | |_| |/ __/| . \ ___) |
  \____|_____|_|\_|____/
 
- This is the main file for all the main fil stuff.
- Don't use non main file stuff for this
+ This is the main file for all the main file stuff.
+ Don't use non main file stuff in this file
 """
 import pyglet
 
-game_window = pyglet.window.Window(800, 600)
+pyglet.resource.path = ['./Assets']
+pyglet.resource.reindex()
+game_window = pyglet.window.Window(1920, 1080)
+
+# Sprites
+background_image = pyglet.resource.image("Backgrounds/Nebula Blue.png")
+
+def centerImage(image):
+    """Sets and image's anchor point to its center"""
+    image.anchor_x = image.width // 2
 
 if __name__ == '__main__':
     pyglet.app.run()
