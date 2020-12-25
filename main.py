eba79d09295987184aee0bda9ecf8897481324f7
Griffin Walraven <griffin.walraven@gmail.com>
2020-12-24 21:56:34 -0800

    made main

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+  ____ ____  _  _______
+ / ___|___ \| |/ |___ /
+| |  _  __) | ' /  |_ \
+| |_| |/ __/| . \ ___) |
+ \____|_____|_|\_|____/
+
+ This is the main file for all the main fil stuff.
+ Don't use non main file stuff for this
+"""
