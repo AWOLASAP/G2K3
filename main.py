5837a7034f35cdb2b811cc4c79b75a50cc800eee
Griffin Walraven <griffin.walraven@gmail.com>
2020-12-24 22:03:04 -0800

    going places

--- main.py
+++ main.py
@@ -8,3 +8,9 @@
  This is the main file for all the main fil stuff.
  Don't use non main file stuff for this
 """
+import pyglet
+
+game_window = pyglet.window.Window(800, 600)
+
+if __name__ == '__main__':
+    pyglet.app.run()
